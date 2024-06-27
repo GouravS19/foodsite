c2d824804f15e5bfd83863e61a92e3ae98425e85
GouravS19 <75998498+GouravS19@users.noreply.github.com>
2024-06-27 07:01:59 +0530

    resmenu first part is done like fetching all the data now the styling part is left

--- src/Components/MenuSection.js
+++ src/Components/MenuSection.js
@@ -3,16 +3,33 @@ import EachListItem from './EachListItem'
 
 const MenuSection=({data})=> {
     return (
-        <div>
+        <div className='bg-red-200 border-4 m-3 border-black'>
             <div className='bg-orange-200'>
                 {data.card.card.title}
             </div>
-            {/* {
-                data.card.card.itemCards.map((item)=>{
-                    return <EachListItem data={item}/>
-                })
-            } */}
-            <EachListItem data={data.card.card.itemCards[0]}/>
+            {
+                data.card.card.itemCards?(
+                    data.card.card.itemCards.map((item,ind)=>{
+                        return <EachListItem data={item} key={ind}/>
+                    })
+                ):(
+                    data.card.card.categories?(
+                        data.card.card.categories.map((ele)=>{
+                            return (ele.itemCards.map((item,ind)=>{
+                                return <EachListItem data={item} key={ind}/>
+                            }))
+                            // return <EachListItem data={ele[0]}/>
+
+                        })
+                        // data.card.card.categories[0].itemCards.map((item)=>{
+                        //         return <EachListItem data={item}/>
+                        // })
+                    ):(
+                        console.log('')
+                    )
+                )
+            }
+            {/* <EachListItem data={data.card.card.itemCards[0]}/> */}
         </div>
     )
 
